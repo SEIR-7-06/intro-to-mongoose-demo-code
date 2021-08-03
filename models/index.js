4518734b36585bf2d9642bc189ba20ad2f870423
msolorio <msolorio.github@gmail.com>
2021-08-03 13:59:45 -0700

    Sets up connection to DB

---
+++ models/index.js
@@ -0,0 +1,20 @@
+// Connect to our Database
+const mongoose = require('mongoose');
+
+// Get the address for the Database
+// http://localhost:4000/fruits
+const connectionString = 'mongodb://localhost:27017/testblog';
+
+// Fire off the connection
+// and deactivate pesky deprication warnings
+mongoose.connect(connectionString, { 
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+	useFindAndModify: false,
+});
+
+// Listen for when we connect, and let us know!
+mongoose.connection.on('connected', () => {
+  console.log('You are connected to MongoDB! 🥭');
+})
